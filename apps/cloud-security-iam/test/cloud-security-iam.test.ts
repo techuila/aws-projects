910fc0b6337ac10eb9724836d4cfe2e62fe6ae0e
techuila <axlcuyugan05@gmail.com>
2025-09-04 23:45:13 +0800

    feat(cloud-security-iam): initial commit

---
+++ apps/cloud-security-iam/test/cloud-security-iam.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CloudSecurityIam from '../lib/cloud-security-iam-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cloud-security-iam-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CloudSecurityIam.CloudSecurityIamStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
